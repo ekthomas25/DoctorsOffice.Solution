444d0aea435553e280dae4de5efe2e022eab28f6
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-05 14:32:34 -0800

    Add constructor and properties.
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>

---
+++ DoctorsOffice/Models/Patient.cs
@@ -0,0 +1,15 @@
+namespace DoctorsOffice.Models
+{
+  public class Patient
+  {
+    public Patient()
+    {
+      this.JoinDoctorPatients = new HashSet<DoctorPatient>();
+    }
+    public int PatientId { get; set; }
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+    public virtual ICollection<DoctorPatient> JoinDoctorPatients { get; set; }
+  }
+}
