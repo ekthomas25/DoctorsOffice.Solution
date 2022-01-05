757ebd0ce9e10bbd973809bbb09c514f25253a7e
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-05 14:32:14 -0800

    Add constructor and properties.
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>

---
+++ DoctorsOffice/Models/Specialty.cs
@@ -0,0 +1,14 @@
+namespace DoctorsOffice.Models
+{
+  public class Specialty
+  {
+    public Specialty()
+    {
+      this.JoinDoctorSpecialties = new HashSet<DoctorSpecialty>();
+    }
+    public int SpecialtyID { get; set; }
+    public string Description { get; set; }
+
+    public virtual ICollection<DoctorSpecialty> JoinDoctorSpecialties{ get; }
+  }
+}
